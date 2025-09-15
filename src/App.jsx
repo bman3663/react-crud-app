8f1d85cf4b5c38e63b325a4b8cf909cf672328b1
bman3663 <bdspin17@gmail.com>
2025-09-16 00:25:10 +0300

    Defined the methods from the context in App.jsx, implemented useEffect to load & save the array of workouts to and from local storage

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import "./app.css";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import About from "./components/About";
+import Help from "./components/Help";
+import Form from "./components/Form";
+import Table from "./components/Table";
+import { useState } from "react";
+import { useEffect } from "react";
+import { WorkoutProvider } from "./contexts/workoutContext";
+
+import {
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+function App() {
+  const [workouts, setWorkouts] = useState([]);
+
+  const addWorkout = (workout) => {
+    setWorkouts((prev) => [{ id: Date.now(), ...workout }, ...prev]);
+  };
+
+  const updateWorkout = (id, newDescription) => {
+    setWorkouts((prev) =>
+      prev.map((prevWorkout) =>
+        prevWorkout.id === id
+          ? { ...prevWorkout, description: newDescription }
+          : prevWorkout
+      )
+    );
+  };
+
+  const deleteWorkout = (id) => {
+    setWorkouts((prev) => prev.filter((workout) => workout.id !== id));
+  };
+
+  useEffect(() => {
+    const savedWorkouts = JSON.parse(localStorage.getItem("workouts"));
+    if (savedWorkouts && savedWorkouts.length > 0) {
+      setWorkouts(savedWorkouts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("workouts", JSON.stringify(workouts));
+  }, [workouts]);
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route path="" element={<Home />} />
+        <Route path="table" element={<Table />} />
+        <Route path="form" element={<Form />} />
+        <Route path="about" element={<About />} />
+        <Route path="help" element={<Help />} />
+      </Route>
+    )
+  );
+
+  return (
+    <WorkoutProvider
+      value={{
+        workouts,
+        addWorkout,
+        updateWorkout,
+        deleteWorkout,
+      }}
+    >
+      <RouterProvider router={router} />
+    </WorkoutProvider>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import App from "./App";
 import "./index.css";
-// import { Layout, Home, About, Help, Table } from "./components";
-import Layout from "./components/Layout";
-import Home from "./components/Home";
-import About from "./components/About";
-import Help from "./components/Help";
-import Form from "./components/Form";
-import Table from "./components/Table";
-
-import {
-  Route,
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
-} from "react-router-dom";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="" element={<Home />} />
-      <Route path="table" element={<Table />} />
-      <Route path="form" element={<Form />} />
-      <Route path="about" element={<About />} />
-      <Route path="help" element={<Help />} />
-    </Route>
-  )
-);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <App />
   </StrictMode>
 );
