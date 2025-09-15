9466ca1283aba0b06b7866786d1fc4644372e31d
bman3663 <bdspin17@gmail.com>
2025-09-15 22:28:40 +0300

    Setup basic routes to components using react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import { Layout, Home, About, Help, Table } from "./components";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import About from "./components/About";
+import Help from "./components/Help";
+import Form from "./components/Form";
+import Table from "./components/Table";
 
-createRoot(document.getElementById('root')).render(
+import {
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="table" element={<Table />} />
+      <Route path="form" element={<Form />} />
+      <Route path="about" element={<About />} />
+      <Route path="help" element={<Help />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
