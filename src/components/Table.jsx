7f0b9dcf176562ea178e62e1999cd1d4b7e2bee3
bman3663 <bdspin17@gmail.com>
2025-09-16 17:54:37 +0300

    created workoutItem, displaying workout info, and implementing updateWorkout and deleteWorkout methods

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,29 +1,27 @@
 import React from "react";
 import { useWorkout } from "../contexts/workoutContext";
 import WorkoutItem from "./WorkoutItem";
+
 function Table() {
   const { workouts, updateWorkout, deleteWorkout } = useWorkout();
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Description</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {workouts.map((workout) => (
-          <WorkoutItem
-            key={workout.id}
-            workout={workout}
-            onEdit={updateWorkout}
-            onDelete={deleteWorkout}
-          />
-        ))}
-      </tbody>
-    </table>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {workouts.map((workout) => (
+            <WorkoutItem key={workout.id} workout={workout} />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
--- src/components/WorkoutItem.jsx
+++ src/components/WorkoutItem.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { useWorkout } from "../contexts/workoutContext";
+import { useState } from "react";
+function WorkoutItem({ workout }) {
+  const [description, setDescription] = useState(workout.description);
+  const [editable, setEditable] = useState(false);
+  const { updateWorkout, deleteWorkout } = useWorkout();
 
-function WorkoutItem() {
-  return <div>WorkoutItem</div>;
+  const editWorkout = () => {
+    updateWorkout(workout.id, { ...workout, description });
+    setEditable(false);
+  };
+  return (
+    <tr>
+      <td>{workout.name}</td>
+      <td>
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          readOnly={!editable}
+        />
+      </td>
+      <td>
+        <button
+          onClick={() => {
+            if (editable) {
+              editWorkout();
+            } else setEditable((prev) => !prev);
+          }}
+        >
+          {" "}
+          {editable ? "save" : "edit"}
+        </button>
+      </td>
+      <td>
+        <button className="" onClick={() => deleteWorkout(workout.id)}>
+          delete
+        </button>
+      </td>
+    </tr>
+  );
 }
 
 export default WorkoutItem;
